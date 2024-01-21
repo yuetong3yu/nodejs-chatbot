520cc0bf2b1e066af43e8b73e4b9e4f193e57b99
chris.yu <yuetong3yu@gmail.com>
2024-01-21 11:03:08 +1100

    import dotenv and setup ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('1234 hello world')
+import { OpenAI } from 'openai'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+console.log('1234 env', process.env.OPENAI_KEY)
+// const openai = new OpenAI({
+//   apiKey: ''
+// })
